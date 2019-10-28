79ac7864ef655f60c7b0b4ed2ee53070281900a5
袁永华 <282058905@qq.com>
2019-10-28 18:04:22 +0800

    修改bug~

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import request from '@/utils/request.js'
 
 Vue.use(Vuex)
 
+const state =  {
+  baseCourse:[],//课程类别
+}
+
 export default new Vuex.Store({
-  state: {
-
-  },
+  state,
   mutations: {
-
+    setBaseCourse(state,data){
+      state.baseCourse = data
+    }
+  },
+  actions:{
+    getBaseCourse({state,commit}){
+      request({url: '/course/getBaseCourse'})
+        .then(res=> commit(`setBaseCourse`,res.datas))
+    }
   }
 })
-
 
--- src/views/contentManage/Index.vue
+++ src/views/contentManage/Index.vue
@@ -166,9 +166,20 @@
       :title="showAddDialog==='111'?'新建':'修改'" 
       @close="showAddDialog=false" 
       width="600px">
-      <el-form label-width="110px" ref="addForm" :model="addDialogModel" :rules="addDialogRules">
+      <el-form label-width="120px" ref="addForm" :model="addDialogModel" :rules="addDialogRules">
         <el-form-item label="课程名称" prop="name">
           <el-input v-model="addDialogModel.name"></el-input>
+        </el-form-item>
+        <el-form-item label="课程类别" prop="courseTypeList">
+          <!-- <el-input v-model="addDialogModel.name"></el-input> -->
+          <el-cascader
+            style="width:300px;"
+            v-model="addDialogModel.courseTypeList"
+            :options="baseCourse"
+            :props="{
+              value:'id',
+              label:'name'
+            }"></el-cascader>
         </el-form-item>
         <el-form-item label="课程类型">
           <el-select v-model="addDialogModel.type">
@@ -239,6 +250,13 @@
               :step="1"></el-input-number>
             <span class="tip">购买1个单位产品所反积分</span>
           </el-form-item>
+          <el-form-item label="邀请者所得积分" prop="buyTypeList[0].inviteIntegration">
+            <el-input-number 
+              :min="0"
+              v-model="addDialogModel.buyTypeList[0].inviteIntegration" 
+              :step="1"></el-input-number>
+            <span class="tip">购买1个单位产品邀请者可获得的积分</span>
+          </el-form-item>
           <p class="sTitle">按天购买</p>
           <el-form-item label="购买价格" prop="buyTypeList[1].money">
             <el-input-number
@@ -252,6 +270,13 @@
               v-model="addDialogModel.buyTypeList[1].integration" 
               :step="1"></el-input-number>
             <span class="tip">购买1个单位产品所反积分</span>
+          </el-form-item>
+          <el-form-item label="邀请者所得积分" prop="buyTypeList[1].inviteIntegration">
+            <el-input-number 
+              :min="0"
+              v-model="addDialogModel.buyTypeList[1].inviteIntegration" 
+              :step="1"></el-input-number>
+            <span class="tip">购买1个单位产品邀请者可获得的积分</span>
           </el-form-item>
         </template>
         <el-row class="btnwrap">
@@ -270,6 +295,10 @@
           <el-form-item label="课节名称" prop="name">
             <el-input v-model="courseVos.name"></el-input>
           </el-form-item>
+          <el-form-item label="可试看时长(秒)" prop="freeSecond">
+            <el-input-number v-model="courseVos.freeSecond" :step="1" :min="0"></el-input-number>
+            <span class="tip">输入0表示不可试看该节</span>
+          </el-form-item>
           <el-form-item label="课节文件">
             <input type="file" @change="beforeKJUpload"/>
             <p v-if="courseVos.url">{{courseVos.url}}</p>
@@ -285,6 +314,7 @@
 
 <script>
 import {$getCourse,$saveFile,$addOrEditCourse,$setCourseStatus} from '@/api/index';
+import { mapState } from 'vuex'
 import Page from '@/components/Page';
 
 export default {
@@ -296,6 +326,9 @@ export default {
         name:[
           { required: true, message: '请输入课程名称', trigger: 'blur' },
           { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
+        ],
+        freeSecond:[
+          { required: true, message: '不可为空', trigger: 'change' },
         ]
       },
       courseType:"",
@@ -306,6 +339,9 @@ export default {
       showAddDialog:false,
       uploadedList:[{file:'dasdasdas.mp4',documentName:""}],
       addDialogRules:{
+        courseTypeList:[
+          { required: true, message: '请选择课程类别', trigger: 'change' }
+        ],
         name:[
           { required: true, message: '请输入课程名称', trigger: 'blur' },
           { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
@@ -318,6 +354,9 @@ export default {
             integration:[
               { required: true, message: '请输入所反积分', trigger: 'change' }
             ],
+            inviteIntegration:[
+              { required: true, message: '请输入所反积分', trigger: 'change' }
+            ],
           },
           {
             money:[
@@ -326,10 +365,16 @@ export default {
             integration:[
               { required: true, message: '请输入所反积分', trigger: 'change' }
             ],
+            inviteIntegration:[
+              { required: true, message: '请输入所反积分', trigger: 'change' }
+            ],
           }
         ]
       },
       addDialogModel:{
+        courseTypeList:null,//存储课程类别的数组
+        courseTypeStr:"",//服务器存储课程类别的字符串
+        courseTypeId:'',//服务器存储课程类别的id
         name:'课程名称',
         type:1,//课程类型
         isfree:1,//是否免费
@@ -341,22 +386,41 @@ export default {
         imgUrl:"",//课程封面
         buyTypeList:[
           {
-            buyTyp:1,
+            buyType:1,
             money:0,//价格
             integration:0,//购买所得积分
+            inviteIntegration:0,
           },
           {
-            buyTyp:2,
+            buyType:2,
             money:0,//价格
             integration:0,//购买所得积分
+            inviteIntegration:0,
           }
         ]
       }
     }
   },
+  computed:{
+    ...mapState({
+      baseCourse:"baseCourse"
+    })
+
+  },
   mounted(){
     //获取课程列表
     this.getCourse();
+
+    //获取课程类别
+    this.$store.dispatch("getBaseCourse")
+  },
+  watch:{
+    baseCourse:{
+      handler:function(newV){
+        console.log(newV)
+      },
+      deep:true
+    }
   },
   methods:{
     //获取课程列表
@@ -397,9 +461,11 @@ export default {
           if(this.addDialogModel.imgUrl) {
             let params = {
               ...this.addDialogModel,
-              id:this.showUpDialog === '111'?'':this.showUpDialog
+              id:this.showUpDialog === '111'?'':this.showUpDialog,
+              courseTypeStr:this.addDialogModel.courseTypeList.join(','),//服务器存储课程类别的字符串
+              courseTypeId:this.addDialogModel.courseTypeList[this.addDialogModel.courseTypeList.length-1],//服务器存储课程类别的id
             }
-            $addOrEditCourse(this.addDialogModel)
+            $addOrEditCourse(params)
               .then(res=> {
                 if(!res.success) return
                 this.getCourse();
@@ -443,26 +509,29 @@ export default {
         documentName:'img',
         file:formData
       }).then(res=> {
-        this.addDialogModel.imgUrl = res.datas.ShowUrl
+        this.addDialogModel.imgUrl = res.datas
       })
     },
     //修改某条信息
     editRow(row){
       this.showAddDialog = row.id
       let buyTypeList = row.buyTypeList
+      let courseTypeList = [];
       if(row.buyTypeList.length && row.buyTypeList.length !== 2) {
         row.buyTypeList.map(item => {
           if(item.buyType == 1) {
             buyTypeList.push({
-              buyTyp:2,
+              buyType:2,
               money:0,//价格
               integration:0,//购买所得积分
+              inviteIntegration:0,
             })
           }else {
             buyTypeList.unshift({
-              buyTyp:1,
+              buyType:1,
               money:0,//价格
               integration:0,//购买所得积分
+              inviteIntegration:0,
             })
           }
         })
@@ -470,21 +539,30 @@ export default {
       if(!row.buyTypeList.length) {
         buyTypeList = [
           {
-            buyTyp:1,
+            buyType:1,
             money:0,//价格
             integration:0,//购买所得积分
+            inviteIntegration:0,
           },
           {
-            buyTyp:2,
+            buyType:2,
             money:0,//价格
             integration:0,//购买所得积分
+            inviteIntegration:0,
           }
         ]
       }
+      if(row.courseTypeStr) {
+         row.courseTypeStr.split(',').map(item=> {
+           courseTypeList.push(item*1)
+         })
+      }
       this.addDialogModel = {
         ...row,
-        buyTypeList
+        buyTypeList,
+        courseTypeList
       }
+      console.log(this.addDialogModel)
     },
     //修改上架下架状态
     setCourseStatus(id,status){
@@ -511,7 +589,6 @@ export default {
     },
     //添加课程节数
     addNode(row) {
-      console.log(row)
       this.currentNode = row;
       this.courseVos = {
         name:'',
@@ -534,7 +611,7 @@ export default {
         documentName:tp[this.courseVos.type-1],
         file:formData
       }).then(res=> {
-        this.$set(this.courseVos,'url',res.datas.ShowUrl)
+        this.$set(this.courseVos,'url',res.datas)
       })
     },
     //添加课节
@@ -546,7 +623,15 @@ export default {
           }else {
             const currentNode = this.currentNode;
             const courseVos = currentNode.courseVos;
-            courseVos.push(this.courseVos);
+            const oldCourseVos = JSON.parse(JSON.stringify(currentNode)).courseVos;
+            // ---
+            // this.courseVos
+            const idx = courseVos.findIndex(el=> el.id === this.courseVos.id)
+            if( idx!== -1){
+              courseVos[idx] = this.courseVos
+            }else{
+              courseVos.push(this.courseVos);
+            }
             this.$set(this.currentNode,'courseVos',courseVos)
             $addOrEditCourse(currentNode)
               .then(res=> {
@@ -555,13 +640,15 @@ export default {
                 this.currentNode = null
                 this.courseVos = null
               })
+              .catch(res=> {
+                this.$set(this.currentNode,'courseVos',oldCourseVos)
+              })
           }
         }
       })
     },
     //编辑课节
     editNode(n,row) {
-      console.log(n)
       this.courseVos = n
       this.currentNode = row
     },
